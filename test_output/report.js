d2078ea4a3810480a77266350ec84d53b5d66301
ravidkone <ravidkone@gmail.com>
2020-06-07 19:22:15 +0530

    changes in master

--- test_output/report.js
+++ test_output/report.js
@@ -10,10 +10,7 @@ formatter.scenario({
   "keyword": "Scenario",
   "tags": [
     {
-      "name": "@Login"
-    },
-    {
-      "name": "@test"
+      "name": "@DataTable"
     }
   ]
 });
@@ -21,11 +18,9 @@ formatter.step({
   "name": "user already logged into web",
   "keyword": "Given "
 });
-formatter.match({
-  "location": "cucumber.BDD.contactStepDefinition.user_already_logged_into_web()"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.step({
   "name": "user enter contact details as FirstName and LastName",
@@ -34,21 +29,17 @@ formatter.step({
   ],
   "keyword": "Then "
 });
-formatter.match({
-  "location": "cucumber.BDD.contactStepDefinition.user_enter_contact_details_as_FirstName_and_LastName(io.cucumber.datatable.DataTable)"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.step({
   "name": "user click on contact submit tab",
   "keyword": "When "
 });
-formatter.match({
-  "location": "cucumber.BDD.contactStepDefinition.user_click_on_contact_submit_tab()"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.step({
   "name": "user fill cost details",
@@ -57,30 +48,24 @@ formatter.step({
   ],
   "keyword": "Then "
 });
-formatter.match({
-  "location": "cucumber.BDD.contactStepDefinition.user_fill_cost_details(io.cucumber.datatable.DataTable)"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.step({
   "name": "form contact list will be added",
   "keyword": "Then "
 });
-formatter.match({
-  "location": "cucumber.BDD.contactStepDefinition.form_contact_list_will_be_added()"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 formatter.step({
   "name": "user contact is added",
   "keyword": "Then "
 });
-formatter.match({
-  "location": "cucumber.BDD.contactStepDefinition.user_contact_is_added()"
-});
+formatter.match({});
 formatter.result({
-  "status": "passed"
+  "status": "undefined"
 });
 });
